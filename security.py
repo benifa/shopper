ad5df5dcf245f738c8a393c9a518b72aa58a6fd9
benifabrice <karame13>
2017-01-10 22:31:58 -0600

    new Fix

--- security.py
+++ security.py
@@ -2,18 +2,12 @@ from werkzeug.security import safe_str_cmp
 
 from theCode.models.usermodel import UserModel
 
-# users = [
-#     User(1, "beni", "beni")
-# ]
-#
-# username_mapping = {u.username : u  for u in users}
-#
-# userid_mapping = {u.id : u  for u in users}
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if user and safe_str_cmp(user.password,password):
+    if user and safe_str_cmp(user.password, password):
         return user
+
 
 def identify(payload):
     user_id = payload["identity"]
